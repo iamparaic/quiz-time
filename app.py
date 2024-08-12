9f307c7af3155bbd0fcc02f1fcbd1035d5c3fda7
Paraic <iamparaic@gmail.com>
2024-08-12 20:01:15 +0100

    Update app.py
    incorrect syntax, error correction

--- app.py
+++ app.py
@@ -13,13 +13,13 @@ if __name__ == '__main__':
 QUESTIONS = [
     {"question": "What is the capital of France?", "options": ["Paris", "London", "Berlin"], "answer": "Paris"},
     {"question": "What is 2 + 2?", "options": ["3", "4", "5"], "answer": "4"}
-    {"question": "Which element on the periodic table has the chemical symbol O?": ["Oxygen", "Gold", "Hydrogen"], "answer": "Oxygen"}
-    {"question": "Which planet in our solar system is known as the Red Planet?": ["Venus", "Mars", "Jupiter"], "answer": "Mars"}
-    {"question": "What is the smallest prime number?": ["0", "1", "2"], "answer": "2"}
-    {"question": "Who wrote the novel 1984?": ["George Orwell", "Ronan Berry", "Ray Bradbury"], "answer": "George Orwell"}
-    {"question": "In which year did the Titanic sink?": ["1910", "1911", "1912"], "answer": "1912"}
-    {"question": "Which element has the atomic number 79": ["Gold", "Silver", "Mercury"], "answer": "Gold"}
-    {"question": "Which mathematician is credited with the invention of the modern binary system?": ["Blaise Pascal", "Gottfried Wilhelm Leibniz", "René Descartes"], "answer": "Gottfried Wilhelm Leibniz"}
+    {"question": "Which element on the periodic table has the chemical symbol O?", "options": ["Oxygen", "Gold", "Hydrogen"], "answer": "Oxygen"}
+    {"question": "Which planet in our solar system is known as the Red Planet?", "options": ["Venus", "Mars", "Jupiter"], "answer": "Mars"}
+    {"question": "What is the smallest prime number?", "options": ["0", "1", "2"], "answer": "2"}
+    {"question": "Who wrote the novel 1984?", "options": ["George Orwell", "Ronan Berry", "Ray Bradbury"], "answer": "George Orwell"}
+    {"question": "In which year did the Titanic sink?", "options": ["1910", "1911", "1912"], "answer": "1912"}
+    {"question": "Which element has the atomic number 79", "options": ["Gold", "Silver", "Mercury"], "answer": "Gold"}
+    {"question": "Which mathematician is credited with the invention of the modern binary system?", "options": ["Blaise Pascal", "Gottfried Wilhelm Leibniz", "René Descartes"], "answer": "Gottfried Wilhelm Leibniz"}
 ]
 
 @app.route('/')
